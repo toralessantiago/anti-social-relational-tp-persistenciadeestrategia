33db19793babd2e8e5e9f3261d7349e4e12bc90b
toralessantiago <toralessantiago885@gmail.com>
2026-05-27 21:13:56 -0300

    feat: carpetas routes, controllers, middlewares y schemas

--- models/user.js
+++ models/user.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -10,19 +8,33 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.hasMany(models.Post,{foreignKey:'userNickname', as:'posts'})
-      User.hasMany(models.Comment,{foreignKey:'userNickname',as:'comments'})
-      User.belongsToMany(models.User, {through:'Follow',foreignKey:'followingId', as:'followers'})
-      User.belongsToMany(models.User, {through:'Follow',foreignKey:'followerId', as:'following'})
+      User.hasMany(models.Post, { foreignKey: "userNickname", as: "posts" });
+      User.hasMany(models.Comment, {
+        foreignKey: "userNickname",
+        as: "comments",
+      });
+      User.belongsToMany(models.User, {
+        through: "Follow",
+        foreignKey: "followingId",
+        as: "followers",
+      });
+      User.belongsToMany(models.User, {
+        through: "Follow",
+        foreignKey: "followerId",
+        as: "following",
+      });
     }
   }
-  User.init({
-    nickName: { type:DataTypes.STRING, unique: true, allowNull:false },
-    email: {type: DataTypes.STRING, allowNull:false},
-    password: {type: DataTypes.STRING, allowNull:false}
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      nickName: { type: DataTypes.STRING, unique: true, allowNull: false },
+      email: { type: DataTypes.STRING, allowNull: false },
+      password: { type: DataTypes.STRING, allowNull: false },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    },
+  );
   return User;
 };
